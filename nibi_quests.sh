5061160a7f84da37bc2507238ccfbac08128b762
Dinh Chau Nguyen <nguyendinhchau795@gmail.com>
2023-03-05 22:52:34 +0700

    Update list validator

--- nibi_quests.sh
+++ nibi_quests.sh
@@ -7,12 +7,14 @@ redelegate_arr[0]="nibivaloper1s4xk8sq06e0jpf4uf2xatwfsx8vg5sr03jm297"
 redelegate_arr[1]="nibivaloper1khwntys59nwxl906p8rl68ky3d5tzuk776mts9"
 redelegate_arr[2]="nibivaloper1pnqhamqueyjpcc9yrg0a66exz6hjvat9gjvkr9"
 redelegate_arr[3]="nibivaloper1vnr2ek0x8f0g9cqurzm9nhhcmdagfe0fh77k3u"
-redelegate_arr[4]="nibivaloper1rkavq8n08wmjxhvsmpgsxc9xdjsragn0z09sv8"
-redelegate_arr[5]="nibivaloper1kr0pqvphjwvau578m53ps8llaym0swp4dljmee"
-redelegate_arr[6]="nibivaloper1p5n4cfajhwf5xpemc3zxad3nwsy267nxqtlyw2"
-redelegate_arr[7]="nibivaloper1gc6vpl9j0ty8tkt53787zps9ezc70kj8wv386g"
-redelegate_arr[8]="nibivaloper1vnr2ek0x8f0g9cqurzm9nhhcmdagfe0fh77k3u"
-redelegate_arr[9]="nibivaloper19v87lysa6jk6wrw0xlfh7y92czz7gnxyuw9uj4"
+redelegate_arr[4]="nibivaloper195w5wxp8hgqgz7schdukq7u5kadc2tnrsc00a0"
+redelegate_arr[5]="nibivaloper1e04un8tgxe9zm5630jg08p0uf5x024lp79t4u8"
+redelegate_arr[6]="nibivaloper1gz79ft45w335xg0p0fst8nlz30clwadclr5uwy"
+redelegate_arr[7]="nibivaloper1e04un8tgxe9zm5630jg08p0uf5x024lp79t4u8"
+redelegate_arr[8]="nibivaloper15t60ugdvgsapkp779h4eanx0jjcef9vkva8m34"
+redelegate_arr[9]="nibivaloper15xa3kcsq8mnvv6lqsgtaa7kmhl6938lm2se8an"
+redelegate_arr[10]="nibivaloper1xt7q9pd4m3mp50kxzj9wsuu7t2xmlgvc66d5m3"
+
 printHeader
 
 for i in 1 2 3 4
